a708a16245b0a38d8d975301c2bd2f5ce3651b70
Fayimora Femi-Balogun <fayi@fayimora.com>
2016-12-16 18:31:48 +0000

    Turn torrents into list so we can inspect

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from crawlers.one337x import One337x
 
 if __name__ == '__main__':
     crawler = One337x()
-    torrents = crawler.fetch_torrents('arrow')
+    torrents = list(crawler.fetch_torrents('arrow'))
