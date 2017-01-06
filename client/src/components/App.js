29bf14ffbc9b31507c8a6e13355f3a0e60da8a03
Fayimora Femi-Balogun <fayi@fayimora.com>
2017-01-06 01:15:06 +0000

    Setup axios for fetching data from API :wink:

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      torrents: []
+    };
+  }
+
+  componentDidMount() {
+    this.search('designated');
+  }
+
+  search = (searchTerm) => {
+    axios.get(`http://localhost:8090/search/${searchTerm}`)
+      .then(res => {
+        this.setState({
+          torrents: res.data
+        });
+      });
+  }
+
   render () {
+    const torrents = this.state.torrents;
     return (
       <div className="container">
         <div className="header clearfix">
@@ -18,8 +40,17 @@ class App extends React.Component {
           <h1>Welcome to Osprey!!!</h1>
         </div>
 
+        <h2>My Data</h2>
+        <div className="container">
+          <ul>
+            {torrents.map(torrent =>
+              <li key={torrent.id}>{torrent.name}</li>
+            )}
+          </ul>
+        </div>
+
         <footer className="footer">
-          <p>&copy; 2016 Fayimora</p>
+          <p>&copy; 2017 Fayimora</p>
         </footer>
       </div>
     );
